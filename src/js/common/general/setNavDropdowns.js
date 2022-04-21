e63095ee483bc27950772c1ea3c7d028b697e803
artemats <artem.matseruk@gmail.com>
2022-04-21 23:20:48 +0300

    fix: (General) fix toggle nav dropdowns on phones

--- src/js/common/general/setNavDropdowns.js
+++ src/js/common/general/setNavDropdowns.js
@@ -2,11 +2,8 @@ export const setNavDropdowns = () => {
 	const dropdowns = document.querySelectorAll('.nav li ul');
 	if (!!dropdowns.length) {
 		for (let i = 0; i < dropdowns.length; i++) {
-			let parent = dropdowns[i].previousElementSibling;
 			dropdowns[i].parentNode.classList.add('dropdown');
-			parent.classList.contains('dropdown-link')
-				? parent.classList.remove('dropdown-link')
-				: parent.classList.add('dropdown-link');
+			dropdowns[i].previousElementSibling.classList.add('dropdown-link');
 		}
 	}
 };
--- src/js/common/general/toggleNavDropdown.js
+++ src/js/common/general/toggleNavDropdown.js
@@ -1,9 +1,12 @@
 export const toggleNavDropdown = () => {
-	const dropdownLinks = document.querySelectorAll('.dropdown-link');
-	for (let i = 0; i < dropdownLinks.length; i++) {
-		dropdownLinks[i].addEventListener('click', (e) => {
-			e.preventDefault();
-			dropdownLinks[i].parentNode.classList.toggle('is-active');
-		});
+	if (window.innerWidth <= 991) {
+		const dropdownLinks = document.querySelectorAll('.dropdown-link');
+		for (let i = 0; i < dropdownLinks.length; i++) {
+			dropdownLinks[i].addEventListener('click', (e) => {
+				e.preventDefault();
+				let parent = dropdownLinks[i].parentNode;
+				parent.classList.toggle('is-active');
+			});
+		}
 	}
 };
