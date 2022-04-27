a1ecb46201e69d288872e945e6cbf91844e2f8c8
artemats <artem.matseruk@gmail.com>
2022-04-27 22:56:22 +0300

    feat: (General) add data protection page

--- src/js/common/general/toggleNavDropdown.js
+++ src/js/common/general/toggleNavDropdown.js
@@ -1,12 +1,14 @@
 export const toggleNavDropdown = () => {
 	const dropdownLinks = document.querySelectorAll('.dropdown-link');
-	for (let i = 0; i < dropdownLinks.length; i++) {
-		dropdownLinks[i].addEventListener('click', (e) => {
-			e.preventDefault();
-			let parent = dropdownLinks[i].parentNode;
-			parent.classList.contains('is-active')
-				? parent.classList.remove('is-active')
-				: parent.classList.add('is-active');
-		});
+	if (window.innerWidth <= 991) {
+		for (let i = 0; i < dropdownLinks.length; i++) {
+			dropdownLinks[i].addEventListener('click', (e) => {
+				e.preventDefault();
+				let parent = dropdownLinks[i].parentNode;
+				parent.classList.contains('is-active')
+					? parent.classList.remove('is-active')
+					: parent.classList.add('is-active');
+			});
+		}
 	}
 };
